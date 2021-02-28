5300ff996f16333c5cdb0c9577ed41a7a03a053e
Samir Talic <samir.talic@metaforce.se>
2021-02-28 18:12:40 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './images/talgalogo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
       
         <img src={logo} className="App-logo" alt="logo" />
     
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
+
