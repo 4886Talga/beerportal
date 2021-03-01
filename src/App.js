6f8cfb1590b6dd32cf0f6efde713d2ef9b3f7bd0
Samir Talic <samir.talic@metaforce.se>
2021-03-01 18:46:28 +0100

    Add responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import logo from './images/talgalogo.png';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './Navbar';
 
-import React, { Component } from 'react'
-
-export class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-      
-        <img src={logo} className="App-logo" alt="logo" />
-    
-    </div>
+      <div>
+        <Navbar />
+      </div>
     )
   }
 }
