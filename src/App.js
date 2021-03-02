f768631752da10e6bd704a90b6cfb04d64a6d3f0
Samir Talic <samir.talic@metaforce.se>
2021-03-02 10:30:48 +0100

    Add form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './images/talgalogo.png';
 import React, { Component } from 'react';
+import Form from './Form';
 import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <>
         <Navbar />
-      </div>
+        <Form />
+      </>
     )
   }
 }
